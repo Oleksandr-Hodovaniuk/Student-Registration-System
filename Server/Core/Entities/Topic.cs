8ede8f8e2c2eb3b4de16769ec5947279384b7de4
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-19 19:58:01 +0300

    Create Course&Topic entities

---
+++ Server/Core/Entities/Topic.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public class Topic
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = default!;
+        public IEnumerable<Course> Courses { get; set; } = default!;
+    }
+}
