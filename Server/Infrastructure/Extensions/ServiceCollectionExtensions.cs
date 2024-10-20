12b7ef810fac3a68d24d8ec9a06aaf5fd9486520
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-20 21:57:12 +0300

    Change comments

--- Server/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ Server/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -7,7 +7,7 @@ namespace Infrastructure.Extensions
 {
     public static class ServiceCollectionExtensions
     {
-        //The extension method to add custom services to IServiceCollection and connect to a database.
+        //An extension method to add services to the container and connect to a database.
         public static void AddInfractructure(this IServiceCollection services, IConfiguration configuration)
         {
 
