f9df36fc82f9484a0984e55cd18b8f0c72ff03b8
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-20 15:31:07 +0300

    Implement a new approach to a db connection

---
+++ Server/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        //The extension method to add custom services to IServiceCollection and connect to a database.
+        public static void AddInfractructure(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            var connectionString = configuration.GetConnectionString("StudentRegistrationSystemDb"); //Get connection string from a JSON file.
+
+            services.AddDbContext<StudentRegistrationSystemDbContext>(options => 
+                options.UseSqlServer(connectionString));
+        }
+    }
+}
