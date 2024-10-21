957b452c2f6b9945855bb9b723cc4fc735668e22
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-21 19:04:23 +0300

    Test

--- Server/StudentRegistrationSystem.API/Program.cs
+++ Server/StudentRegistrationSystem.API/Program.cs
@@ -11,7 +11,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddInfractructure(builder.Configuration);
 
 var app = builder.Build();
-//HELLO
+
 // Configures the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
