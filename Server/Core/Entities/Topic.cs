17850498150dab1d179d50726a5f4c3cfcb2624f
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-24 13:18:33 +0300

    Create Topic entity

---
+++ Server/Core/Entities/Topic.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities;
+
+public class Topic
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = default!;
+    public IEnumerable<Course> Courses { get; set; } = default!;
+}
