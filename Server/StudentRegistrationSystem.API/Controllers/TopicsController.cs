1b00f8fb56bdffe6f0e363d98adef3569095fe4d
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-24 20:55:55 +0300

    Create HttpGet endpoint

---
+++ Server/StudentRegistrationSystem.API/Controllers/TopicsController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StudentRegistrationSystem.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TopicsController : ControllerBase
+{
+    private readonly ITopicService _service;
+    public TopicsController(ITopicService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetAllAsync()
+    {
+        var topics = await _service.GetAllAsync();
+
+        if (topics == null || !topics.Any())
+        {
+            return NotFound("No topics found.");
+        }
+
+        return Ok(topics);
+    }
+}
