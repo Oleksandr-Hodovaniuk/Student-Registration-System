ed0b6da6a28211a238788929626cc34d929ec23e
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-24 14:08:13 +0300

    Configuring topic entity

---
+++ Server/Infrastructure/Persistence/Configurations/TopicConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+internal class TopicConfiguration : IEntityTypeConfiguration<Topic>
+{
+    public void Configure(EntityTypeBuilder<Topic> builder)
+    {
+        builder.HasKey("CourseId", "TopicId");
+
+        builder.HasMany(t => t.Courses)
+            .WithMany(c => c.Topics)
+            .UsingEntity<Dictionary<string, object>>(   //Configuring a junction table.
+                "CourseTopic",
+                j => j.HasOne<Course>().WithMany().HasForeignKey("CourseId"),
+                j => j.HasOne<Topic>().WithMany().HasForeignKey("TopicId")
+            );
+    }
+}
