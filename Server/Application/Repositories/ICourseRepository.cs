678504e6a4d68b49ddf709ce9534a5de38b6c9f8
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-25 13:29:29 +0300

    Create Course repository

---
+++ Server/Application/Repositories/ICourseRepository.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities;
+
+namespace Application.Repositories;
+
+public interface ICourseRepository
+{
+    public Task<IEnumerable<Course>> GetAllAsync();
+    public Task<IEnumerable<Course>> GetAllAsync(int topicId);
+    public Task<Course?> GetAsync(int id);
+    public Task CreateAsync(Course course);
+    public Task UpdateAsync(Course course);
+    public Task DeleteAsync(int id);
+}
