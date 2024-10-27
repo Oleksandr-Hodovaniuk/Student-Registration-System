c0a4d23f9be7ce3c8c54c746041f0e06fee60578
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2024-10-27 23:01:23 +0200

    Create CourseDTO validatior

---
+++ Server/Application/Validators/CourseDTOValidator.cs
@@ -0,0 +1,34 @@
+﻿using Application.DTOs;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class CourseDTOValidator : AbstractValidator<CourseDTO>
+{
+    public CourseDTOValidator()
+    {
+        RuleFor(x => x.Id)
+            .GreaterThanOrEqualTo(0);
+
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(1, 60).WithMessage("Name must be between 1 and 60 characters.");
+
+        RuleFor(x => x.Description)
+            .NotEmpty().WithMessage("Description is required.")
+            .Length(50, 3000).WithMessage("Description must be between 50 and 3000 characters.");
+
+        RuleFor(x => x.Author)
+           .NotEmpty().WithMessage("Author is required.")
+           .Length(1, 60).WithMessage("Author must be between 1 and 60 characters.");
+
+        RuleFor(x => x.Beginning)
+            .NotEmpty().WithMessage("Beginning date is required.")
+            .GreaterThan(DateTime.Now.Date.AddDays(1))
+            .WithMessage("Beginning date must be at least one day in the future.");
+
+        RuleFor(x => x.Duration)
+            .GreaterThanOrEqualTo((short) 1)
+            .WithMessage("Duration must be at least 1 day");
+    }
+}
