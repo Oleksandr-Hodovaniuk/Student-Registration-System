393fecae40ae223dedebe36772185170569606ad
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-01-23 20:24:13 +0200

    Create Course and Topic entites

---
+++ Server/Core/Entities/Course.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Entities;
+
+public class Course
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public string Description { get; set; } = default!;
+    public DateTime Beginning { get; set; }
+    public short Duration { get; set; }
+    public List<Topic> Topics { get; set; } = new();
+}
---
+++ Server/Core/Entities/Topic.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities;
+
+public class Topic
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public List<Course> Courses { get; set; } = new();
+}
