66442e49cec050ebf2db2098b8a7acdd6616b4c6
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-02-17 20:26:48 +0200

    Create TopicDTOs

---
+++ Server/Application/DTOs/Topic/TopicCreateDTO.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.DTOs.Topic;
+
+public class TopicCreateDTO
+{
+    public string Name { get; set; } = default!;
+}
---
+++ Server/Application/DTOs/Topic/TopicDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace Application.DTOs.Topic;
+
+public class TopicDTO
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+}
