45de3dbcad7547de690dd5bd9da8fa0c2eb7f487
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-02-17 21:47:44 +0200

    Create test

---
+++ Tests/StudentRegistrationSystem.BackendTests/Unit/Services/TopicServiceTests.cs
@@ -0,0 +1,39 @@
+﻿using Application.DTOs;
+using Application.Interfaces.Repositories;
+using Application.Services;
+using Core.Entities;
+using FluentAssertions;
+using Moq;
+
+namespace StudentRegistrationSystem.BackendTests.Unit.Services;
+
+public class TopicServiceTests
+{
+    private Mock<ITopicRepository> mockRepository;
+    private TopicService topicService;
+
+    [SetUp]
+    public void SetUp()
+    {
+        mockRepository = new Mock<ITopicRepository>();
+        topicService = new TopicService(mockRepository.Object);
+    }
+
+    [Test]
+    public async Task CreateAsync_WhenTopicCreateDTOValid_ShouldReturnTopicDTO()
+    {
+        // Arrange
+        var createDto = new TopicCreateDTO { Name = "C++" };
+        var mockTopic = new Topic { Id = Guid.NewGuid(), Name = createDto.Name };
+
+        mockRepository.Setup(r => r.CreateAsync(It.IsAny<Topic>())).ReturnsAsync(mockTopic);
+
+        // Act
+        var result = await topicService.CreateAsync(createDto);
+
+        // Assert
+        result.Should().NotBeNull();
+        result.Id.Should().Be(mockTopic.Id);
+        result.Name.Should().Be(createDto.Name);
+    }
+}
