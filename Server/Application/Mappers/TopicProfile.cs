269da1a88be196dd530801e3d77e7c2e8793c199
Oleksandr Hodovaniuk <s.godovanuk@gmail.com>
2025-02-18 13:42:39 +0200

    Create TopicMapper

---
+++ Server/Application/Mappers/TopicProfile.cs
@@ -0,0 +1,14 @@
+﻿using Application.DTOs;
+using AutoMapper;
+using Core.Entities;
+
+namespace Application.Mappers;
+
+public class TopicProfile : Profile
+{
+	public TopicProfile()
+	{
+		CreateMap<Topic, TopicDTO>().ReverseMap();
+		CreateMap<Topic, TopicCreateDTO>().ReverseMap();
+	}
+}
